be5703ef6f3737a8d344958c063d1b0674d7d65a
karlmorand <karljmorand@gmail.com>
2016-07-11 15:58:30 -0400

    setup basic MVC structure and default express route for testing

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-var app = 
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+  res.render('public/index.html')
+})
+
+app.listen(3000, function(){
+  console.log('Server up, listening on 3000');
+})
