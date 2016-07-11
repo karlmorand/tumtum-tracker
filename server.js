d2b945310b47909f68db3799bc81ad20664d82b0
karlmorand <karljmorand@gmail.com>
2016-07-11 17:46:03 -0400

    implemented node-linkedin module to redirect to linkedin login page and authenticate

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
+var Linkedin = require('node-linkedin')('77za1nt3vews0s', 'jWorZ9VYEw5UAuxR', 'http://localhost:3000/linkedin/callback');
+
+var linkedin = Linkedin.init('77za1nt3vews0s')
+
 
 app.use(express.static('public'));
 
 app.get('/', function(req,res){
   res.render('public/index.html')
+})
+
+app.get('/linkedin', function(req, res){
+  var scope = ['r_basicprofile'];
+  Linkedin.auth.authorize(res, scope);
+});
+
+app.get('/linkedin/callback', function(req, res){
+  Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, results){
+    console.log('State: ' +req.query.state);
+    console.log('Code: '+req.query.code);
+    console.log(results);
+    res.redirect('/loggedin');
+  })
+
+app.get('/loggedin', function(req, res){
+  linkedin.people.me(function(err, $in){
+    res.send($in)
+  })
+})
+
+
 })
 
 app.listen(3000, function(){
