41aa5693f78027b0dd29613c37843949647dc4fb
karlmorand <karljmorand@gmail.com>
2016-07-11 23:49:54 -0400

    LinkedIn javascript API will login a user and show an alert with their name

---
+++ Public/js/linkedin.js
@@ -0,0 +1,22 @@
+function onLoad(){
+  IN.Event.on(IN, "auth", getProfileData);
+}
+
+function onSuccess(data) {
+  console.log(data);
+  alert('Welcome ' + data.firstName)
+}
+
+function onError(error){
+  console.log(error);
+}
+
+function getProfileData(){
+  IN.API.Raw("/people/~").result(onSuccess).error(onError);
+}
+
+function logoutUser(){
+  IN.User.logout(function(){
+    console.log('Logged out');
+  });
+}
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
-var Linkedin = require('node-linkedin')('77za1nt3vews0s', 'jWorZ9VYEw5UAuxR', 'http://localhost:3000/linkedin/callback');
-// var IN = require('//platform.linkedin.com/in.js');
-
-var linkedin = Linkedin.init('77za1nt3vews0s')
+var linkedIn = require('./public/js/linkedin.js')
 
 
 app.use(express.static('public'));
@@ -12,25 +9,6 @@ app.get('/', function(req,res){
   res.render('public/index.html')
 })
 
-app.get('/linkedin', function(req, res){
-  IN.User.authorize(callbackFunction, callbackScope);
-});
-
-// app.get('/linkedin/callback', function(req, res){
-//   Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, results){
-//     console.log('State: ' +req.query.state);
-//     console.log('Code: '+req.query.code);
-//     console.log(results);
-//     res.redirect('/loggedin');
-//   })
-
-// app.get('/loggedin', function(req, res){
-//   linkedin.people.me(function(err, $in){
-//     res.send($in)
-//   })
-// })
-//
-// })
 
 app.listen(3000, function(){
   console.log('Server up, listening on 3000');
