271d782274aa0d94d6ed4fff1fa298a1ab45e235
karlmorand <karljmorand@gmail.com>
2016-07-11 23:14:58 -0400

    Login with LinkedIn button now shows up and opens popup to login

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var Linkedin = require('node-linkedin')('77za1nt3vews0s', 'jWorZ9VYEw5UAuxR', 'http://localhost:3000/linkedin/callback');
+// var IN = require('//platform.linkedin.com/in.js');
 
 var linkedin = Linkedin.init('77za1nt3vews0s')
 
@@ -12,26 +13,24 @@ app.get('/', function(req,res){
 })
 
 app.get('/linkedin', function(req, res){
-  var scope = ['r_basicprofile'];
-  Linkedin.auth.authorize(res, scope);
+  IN.User.authorize(callbackFunction, callbackScope);
 });
 
-app.get('/linkedin/callback', function(req, res){
-  Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, results){
-    console.log('State: ' +req.query.state);
-    console.log('Code: '+req.query.code);
-    console.log(results);
-    res.redirect('/loggedin');
-  })
+// app.get('/linkedin/callback', function(req, res){
+//   Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, results){
+//     console.log('State: ' +req.query.state);
+//     console.log('Code: '+req.query.code);
+//     console.log(results);
+//     res.redirect('/loggedin');
+//   })
 
-app.get('/loggedin', function(req, res){
-  linkedin.people.me(function(err, $in){
-    res.send($in)
-  })
-})
-
-
-})
+// app.get('/loggedin', function(req, res){
+//   linkedin.people.me(function(err, $in){
+//     res.send($in)
+//   })
+// })
+//
+// })
 
 app.listen(3000, function(){
   console.log('Server up, listening on 3000');
