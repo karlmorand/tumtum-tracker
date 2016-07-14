4d96d4a47dd98389ec02fd78bfe60f76b558c4a6
karlmorand <karljmorand@gmail.com>
2016-07-14 11:31:48 -0400

    created basic schema for storing user info in userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+  linkedInID = String,
+  goals = [],
+  jobs = [],
+  companies = [],
+  resumeLink = String,
+  brandStatement = String
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
